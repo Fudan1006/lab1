d3dbcca319af38addbfb9d452ca5d56d87080e10
wangxin <14302010064@fudan.edu.cn>
2016-03-13 16:37:26 +0800

    add a new beverage -- Decaf

---
+++ Lab1/src/lab1/Decaf.java
@@ -0,0 +1,20 @@
+package lab1;
+
+/**
+ * Created by wangxin on 16/3/13.
+ */
+public class Decaf extends CoffeeBeverage {
+    private String description;
+
+    public Decaf() {
+        description = "Decaf";
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double cost() {
+        return super.cost() + 0.5;
+    }
+}
